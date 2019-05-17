2655a388d4fc20a4970bc3a6b86ae25faf260f36
pramesh45 <48338963+pramesh45@users.noreply.github.com>
2019-05-17 20:41:55 +0930

    Signed-off-by: pramesh45 <48338963+pramesh45@users.noreply.github.com>
    Revised verson of MVP

--- java/java.js
+++ java/java.js
@@ -1,9 +1,23 @@
-function openSlideMenu(){
-  document.getElementById('side-menu').style.width = '250px';
-  document.getElementById('main').style.marginLeft = '250px';
+function openSlideMenu() {
+    document.getElementById ('side-menu').style.width = '250px';
+    document.getElementById ('main').style.marginLeft = '250px';
 }
 
-function closeSlideMenu(){
-  document.getElementById('side-menu').style.width = '0';
-  document.getElementById('main').style.marginLeft = '0';
+
+function closeSlideMenu() {
+    document.getElementById ('side-menu').style.width = '0';
+    document.getElementById ('main').style.marginLeft = '0';
 }
+
+const nav_button = document.getElementById('menu_button');
+const sec_nav = document.getElementById('secondary_nav');
+const close = document.getElementById('close_button');
+
+nav_button.addEventListener('click', function(){
+  console.log('working');
+  sec_nav.classList.remove('hidden');
+});
+
+close.addEventListener('click', function(){
+  sec_nav.classList.add('hidden');
+})
